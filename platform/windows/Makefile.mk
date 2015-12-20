2980f2791692a24b20bd4db9882edabcf7009a3f
Norman Zhi <normanzyb@gmail.com>
2015-12-20 07:28:43 +0000

    CC3200 UART programming tool for windows

---
+++ platform/windows/Makefile.mk
@@ -0,0 +1,13 @@
+# Binary programming tool taken from energia-0101E0017
+# If using CC3200 kits from redbearlab.com,
+# add following line to Makefile_platform.mk before including:
+#
+# REDBEARLAB	= yes
+
+PROGCOM	?= 10
+PROGDIR	:= $(dir $(lastword $(MAKEFILE_LIST)))
+ifeq ($(REDBEARLAB), yes)
+PROGRAM	= cd $(PROGDIR); ./cc3200prog.exe $(PROGCOM) $(shell pwd)/$(TRG).bin h
+else
+PROGRAM	= cd $(PROGDIR); ./cc3200prog.exe $(PROGCOM) $(shell pwd)/$(TRG).bin n
+endif
