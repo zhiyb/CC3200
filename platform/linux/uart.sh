0f11e5694eebf9dc8740764455092bbe1c283355
Norman Zhi <normanzyb@gmail.com>
2016-01-30 12:55:27 +0000

    uart.sh sudo

--- platform/linux/uart.sh
+++ platform/linux/uart.sh
@@ -1,9 +1,33 @@
-#!/bin/sh
-if lsmod | fgrep -q ftdi-sio; then
-	:
-else
-	modprobe ftdi-sio
+#!/bin/bash
+
+com="/dev/ttyUSB*"
+
+case "$1" in
+"load")
+	[[ $UID == 0 ]] || (echo "Run as root!" >&2 && exit 1)
+	modprobe ftdi_sio
 	echo 0451 c32a > /sys/bus/usb-serial/drivers/ftdi_sio/new_id
+	exit 0;;
+"unload")
+	if [[ $UID != 0 ]]; then
+		sudo modprobe -r ftdi_sio
+	else
+		modprobe -r ftdi_sio
+	fi
+	exit;;
+esac
+
+(lsmod | fgrep -q ftdi_sio) || (
+	echo "Loading ftdi_sio module..." >&2
+	if [[ $UID != 0 ]]; then
+		sudo $0 load
+	else
+		$0 load
+	fi || exit 1
 	sleep 1
-fi
-microcom -s 115200 -p $(ls /dev/ttyUSB* | tail -n 1)
+)
+
+tty=$(ls $com | tail -n 1)
+[ -z "$tty" ] && echo "Cannot find COM port from $com!" >&2 && exit 1
+echo "Opening COM port $tty..."
+microcom -s 115200 -p "$tty"
